34d412380472681ea25a4fa57609b68fdab33ea5
Stefan Fröse <stefan.froese@udo.edu>
2022-06-21 10:00:23 +0200

    add __init__.py

--- fibonacci/__init__.py
+++ fibonacci/__init__.py
@@ -1,20 +1,4 @@
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument('n', type=int)
-
-
-def fib(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib(n - 1) + fib(n - 2)
-
-
-def main():
-    args = parser.parse_args()
-    print(fib(args.n))
+from fibonacci import fibonacci
 
 if __name__ == '__main__':
-    main()
+    fibonacci()
